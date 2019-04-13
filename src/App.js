c1ed582e1cc15c80ed6e32abfb199263d3ee417f
Gabriel Nogueira <gabrielgoncalvesnogueira@gmail.com>
2019-04-13 20:29:11 -0300

    Aplicacao FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import Routes from './routes'
+// Quando a classe eh no formato stateless, ela pode ser escrita utilizando funcao ou error function
+
+// const App = () => <Main />
+
+function App () {
+    return <Routes />
 }
 
 export default App;
